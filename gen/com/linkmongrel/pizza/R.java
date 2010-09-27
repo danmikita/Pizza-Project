b5e209e677aade8ea2732d8fb7c2904fca8270c0
Dan Mikita <danmikita@gmail.com>
2010-09-27 13:38:27 -0400

    Added hookups for the checkboxes and images. Not yet complete.

--- gen/com/linkmongrel/pizza/R.java
+++ gen/com/linkmongrel/pizza/R.java
@@ -28,20 +28,24 @@ public final class R {
         public static final int sausage=0x7f020006;
     }
     public static final class id {
-        public static final int add_to_cart_button=0x7f070009;
-        public static final int checkout_button=0x7f07000d;
-        public static final int edit_order_button=0x7f07000c;
-        public static final int extra_cheese_checkbox=0x7f070007;
+        public static final int add_to_cart_button=0x7f07000d;
+        public static final int checkout_button=0x7f070011;
+        public static final int edit_order_button=0x7f070010;
+        public static final int extra_cheese_checkbox=0x7f07000b;
         public static final int get_name=0x7f070000;
         public static final int get_started_button=0x7f070001;
-        public static final int list=0x7f07000a;
-        public static final int mushroom_checkbox=0x7f070008;
-        public static final int new_pizza_button=0x7f07000b;
-        public static final int onion_checkbox=0x7f070006;
-        public static final int pepperoni_checkbox=0x7f070003;
-        public static final int peppers_checkbox=0x7f070005;
+        public static final int list=0x7f07000e;
+        public static final int mushroom_checkbox=0x7f07000c;
+        public static final int new_pizza_button=0x7f07000f;
+        public static final int onion_checkbox=0x7f07000a;
+        public static final int onion_image=0x7f070003;
+        public static final int pepperoni_checkbox=0x7f070007;
+        public static final int pepperoni_image=0x7f070004;
+        public static final int peppers_checkbox=0x7f070009;
+        public static final int peppers_image=0x7f070005;
         public static final int pizza_image=0x7f070002;
-        public static final int sausage_checkbox=0x7f070004;
+        public static final int sausage_checkbox=0x7f070008;
+        public static final int sausage_image=0x7f070006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/linkmongrel/pizza/NewWholePizza.java
+++ src/com/linkmongrel/pizza/NewWholePizza.java
@@ -5,14 +5,71 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class NewWholePizza extends Activity implements OnClickListener{
+	
+	private static final int PEPPERONI = 0;
+
+
+
+	private static final int ONION = 1;
+
+
+
+	private static final int PEPPERS = 2;
+
+
+
+	private static final int SAUSAGE = 3;
+
+
+
+	private static final int MUSHROOM = 4;
+
+
+
+	private static final int EXTRA_CHEESE = 5;
+	
+	
+	
+	private ImageView pizzaImage;
+	private ImageView onionImage;
+	private ImageView pepperoniImage;
+	private ImageView peppersImage;
+	private ImageView sausageImage;
+	private CheckBox pepperoniBox;
+	private CheckBox onionBox;
+	private CheckBox peppersBox;
+	private CheckBox sausageBox;
+	private CheckBox cheeseBox;
+	private CheckBox mushroomBox;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.new_pizza);
 		
-		View pizzaImage = findViewById(R.id.pizza_image);
+		// Initialize pizza images.
+		pizzaImage = (ImageView) findViewById(R.id.pizza_image);
+		onionImage = (ImageView) findViewById(R.id.onion_image);
+		pepperoniImage = (ImageView) findViewById(R.id.pepperoni_image);
+		peppersImage = (ImageView) findViewById(R.id.peppers_image);
+		sausageImage = (ImageView) findViewById(R.id.sausage_image);
+		
+		onionImage.setVisibility(0);
+		pepperoniImage.setVisibility(0);
+		peppersImage.setVisibility(0);
+		sausageImage.setVisibility(0);
+		
+		// Initialize checkboxes.
+		pepperoniBox = (CheckBox) findViewById(R.id.pepperoni_checkbox);
+		onionBox = (CheckBox) findViewById(R.id.onion_checkbox);
+		peppersBox = (CheckBox) findViewById(R.id.peppers_checkbox);
+		sausageBox = (CheckBox) findViewById(R.id.sausage_checkbox);
+		mushroomBox = (CheckBox) findViewById(R.id.mushroom_checkbox);
+		cheeseBox = (CheckBox) findViewById(R.id.extra_cheese_checkbox);
 		
 		View getAddToCartButton = findViewById(R.id.add_to_cart_button);
 		getAddToCartButton.setOnClickListener(this);
@@ -26,7 +83,55 @@ public class NewWholePizza extends Activity implements OnClickListener{
 			Intent intent = new Intent(NewWholePizza.this, OrderPage.class);
 			startActivity(intent);
 			break;
+		case R.id.pepperoni_checkbox:
+			displayImage(PEPPERONI);
+			break;
+		case R.id.onion_checkbox:
+			displayImage(ONION);
+			break;
+		case R.id.peppers_checkbox:
+			displayImage(PEPPERS);
+			break;
+		case R.id.sausage_checkbox:
+			displayImage(SAUSAGE);
+			break;
+		case R.id.mushroom_checkbox:
+			displayImage(MUSHROOM);
+			break;
+		case R.id.extra_cheese_checkbox:
+			displayImage(EXTRA_CHEESE);
+			break;
 		// More buttons go here (if any) ...
+		}
+		
+	}
+
+	private void displayImage(int topping) {
+		switch(topping) {
+		case PEPPERONI:
+			if(pepperoniBox.isChecked()) {
+				pepperoniImage.setVisibility(1);
+			} else {
+				pepperoniImage.setVisibility(0);
+			}
+		case ONION:
+			if(pepperoniBox.isChecked()) {
+				onionImage.setVisibility(1);
+			} else {
+				onionImage.setVisibility(0);
+			}
+		case PEPPERS:
+			if(pepperoniBox.isChecked()) {
+				peppersImage.setVisibility(1);
+			} else {
+				peppersImage.setVisibility(0);
+			}
+		case SAUSAGE:
+			if(pepperoniBox.isChecked()) {
+				sausageImage.setVisibility(1);
+			} else {
+				sausageImage.setVisibility(0);
+			}
 		}
 		
 	}
