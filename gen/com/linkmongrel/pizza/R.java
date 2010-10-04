c66dc2408e8342ed2c70d906c07938aaf4b57276
Dan Mikita <danmikita@gmail.com>
2010-10-03 21:25:13 -0400

    Changed the topping selection concept.  Added new images and a new class.

--- gen/com/linkmongrel/pizza/R.java
+++ gen/com/linkmongrel/pizza/R.java
@@ -22,25 +22,34 @@ public final class R {
         public static final int background=0x7f020000;
         public static final int extracheese=0x7f020001;
         public static final int icon=0x7f020002;
-        public static final int mushrooms=0x7f020003;
-        public static final int onion=0x7f020004;
-        public static final int pepperoni=0x7f020005;
-        public static final int peppers=0x7f020006;
-        public static final int pizza=0x7f020007;
-        public static final int sausage=0x7f020008;
+        public static final int mozzarella=0x7f020003;
+        public static final int mushrooms=0x7f020004;
+        public static final int mushrooms2=0x7f020005;
+        public static final int onion=0x7f020006;
+        public static final int onion2=0x7f020007;
+        public static final int pepperoni=0x7f020008;
+        public static final int pepperoni2=0x7f020009;
+        public static final int peppers=0x7f02000a;
+        public static final int peppers2=0x7f02000b;
+        public static final int pizza=0x7f02000c;
+        public static final int sausage=0x7f02000d;
+        public static final int sausage2=0x7f02000e;
     }
     public static final class id {
         public static final int add_to_cart_button=0x7f070013;
-        public static final int checkout_button=0x7f070018;
-        public static final int edit_order_button=0x7f070017;
+        public static final int checkout_button=0x7f070020;
+        public static final int edit_order_button=0x7f07001f;
         public static final int extra_cheese_checkbox=0x7f070010;
         public static final int extracheese_image=0x7f070006;
+        public static final int gallery=0x7f070016;
         public static final int get_name=0x7f070014;
         public static final int get_started_button=0x7f070015;
-        public static final int left_half_button=0x7f070019;
+        public static final int left=0x7f07001a;
+        public static final int left_half_button=0x7f070021;
+        public static final int left_text=0x7f07001b;
         public static final int mushroom_checkbox=0x7f070011;
         public static final int mushrooms_image=0x7f07000b;
-        public static final int new_pizza_button=0x7f070016;
+        public static final int new_pizza_button=0x7f07001e;
         public static final int onion_checkbox=0x7f07000f;
         public static final int onion_image=0x7f070007;
         public static final int pepperoni_checkbox=0x7f07000c;
@@ -48,7 +57,10 @@ public final class R {
         public static final int peppers_checkbox=0x7f07000e;
         public static final int peppers_image=0x7f070009;
         public static final int pizza_image=0x7f070005;
+        public static final int radiogroup=0x7f070017;
+        public static final int right=0x7f07001c;
         public static final int right_half_button=0x7f070012;
+        public static final int right_text=0x7f07001d;
         public static final int rowid=0x7f070000;
         public static final int rowidcolon=0x7f070001;
         public static final int sausage_checkbox=0x7f07000d;
@@ -56,6 +68,8 @@ public final class R {
         public static final int time=0x7f070002;
         public static final int timecolon=0x7f070003;
         public static final int title=0x7f070004;
+        public static final int whole=0x7f070018;
+        public static final int whole_text=0x7f070019;
     }
     public static final class layout {
         public static final int item=0x7f030000;
@@ -63,7 +77,8 @@ public final class R {
         public static final int main=0x7f030002;
         public static final int new_pizza=0x7f030003;
         public static final int order_page=0x7f030004;
-        public static final int right_half=0x7f030005;
+        public static final int pizza_design=0x7f030005;
+        public static final int right_half=0x7f030006;
     }
     public static final class string {
         public static final int add_to_cart_label=0x7f06001e;
@@ -106,4 +121,25 @@ public final class R {
         public static final int thin_label=0x7f060011;
         public static final int whole_label=0x7f060017;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a HelloGallery.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #HelloGallery_android_galleryItemBackground com.linkmongrel.pizza:android_galleryItemBackground}</code></td><td></td></tr>
+           </table>
+           @see #HelloGallery_android_galleryItemBackground
+         */
+        public static final int[] HelloGallery = {
+            0x0101004c
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.linkmongrel.pizza.R.attr#android_galleryItemBackground}
+          attribute's value can be found in the {@link #HelloGallery} array.
+          @attr name android:android_galleryItemBackground
+        */
+        public static final int HelloGallery_android_galleryItemBackground = 0;
+    };
 }
---
+++ src/com/linkmongrel/pizza/ImageAdapter.java
@@ -0,0 +1,54 @@
+package com.linkmongrel.pizza;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+	int mGalleryItemBackground;
+    private Context mContext;
+
+    private Integer[] mImageIds = {
+            R.drawable.pepperoni2,
+            R.drawable.peppers2,
+            R.drawable.onion2,
+            R.drawable.mushrooms2,
+            R.drawable.sausage2,
+            R.drawable.mozzarella,
+    };
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+        TypedArray a = c.obtainStyledAttributes(R.styleable.HelloGallery);
+        mGalleryItemBackground = a.getResourceId(
+                R.styleable.HelloGallery_android_galleryItemBackground, 0);
+        a.recycle();
+    }
+
+    public int getCount() {
+        return mImageIds.length;
+    }
+
+    public Object getItem(int position) {
+        return position;
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+    
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView i = new ImageView(mContext);
+
+        i.setImageResource(mImageIds[position]);
+        i.setLayoutParams(new Gallery.LayoutParams(150, 100));
+        i.setScaleType(ImageView.ScaleType.FIT_XY);
+        i.setBackgroundResource(mGalleryItemBackground);
+
+        return i;
+    }
+}
